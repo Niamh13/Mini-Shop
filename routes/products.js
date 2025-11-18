e024e8246cbc8f8277c0c793aa155c8b56ac7298
Niamh13 <77692255+Niamh13@users.noreply.github.com>
2025-11-18 16:42:44 +0000

    SQL Injection added

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,21 @@
-const express = require("express");
+// shows SQL INJECTION vulnerability - the user input "search" is directly concatenated into the SQL
+
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Products page placeholder");
+const db = new sqlite3.Database('./db/database.sqlite');
+
+router.get('/', (req, res) => {
+    const search = req.query.search || "";
+
+    const sql = "SELECT * FROM products WHERE name LIKE '%" + search + "%'";
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            return res.send("Database error");
+        }
+        res.render('products', { products: rows, search });
+    });
 });
 
 module.exports = router;
